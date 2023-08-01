ba3289106c7c1e9024416fb46458024e68edaee5
Aleksandar Gujas <alexg@diffuser.xyz>
2023-08-01 13:17:57 +0200

    :sparkles: feat: wallet from tutorial fully implemented

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 mod wallet;
-use wallet::eth::{address_from_pubkey, generate_keypair, Wallet};
+use std::env;
 
-fn main() {
+use anyhow::Result;
+use wallet::eth::{address_from_pubkey, establish_web3_connection, generate_keypair, Wallet};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv::dotenv().ok();
     println!("Hello, world!");
     let (private, public) = generate_keypair();
     println!("private and public keys");
@@ -15,4 +20,15 @@ fn main() {
 
     let crypto_wallet = Wallet::new(&private, &public);
     println!("crypto_wallet: {:?}", &crypto_wallet);
+
+    let wallet_file_path = "crypto_wallet.json";
+    crypto_wallet.save_to_file(wallet_file_path)?;
+    let loaded_wallet = Wallet::from_file(wallet_file_path)?;
+    println!("loaded_wallet: {:?}", loaded_wallet);
+
+    let endpoint = env::var("TESTNET_WS")?;
+    let web3_con = establish_web3_connection(&endpoint).await?;
+    let block_number = web3_con.eth().block_number().await?;
+    println!("block number: {}", &block_number);
+    Ok(())
 }
--- src/wallet/eth.rs
+++ src/wallet/eth.rs
@@ -9,13 +9,60 @@ use std::io::BufWriter;
 use std::str::FromStr;
 use std::{fs::OpenOptions, io::BufReader};
 use tiny_keccak::keccak256;
-use web3::types::Address;
+use web3::{
+    transports::{self, WebSocket},
+    types::{Address, TransactionParameters, H256, U256},
+    Web3,
+};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Wallet {
     pub secret_key: String,
     pub public_key: String,
     pub address: String,
+}
+
+impl Wallet {
+    pub fn new(secret_key: &SecretKey, public_key: &PublicKey) -> Self {
+        let addr: Address = address_from_pubkey(&public_key);
+        Wallet {
+            secret_key: secret_key.to_string(),
+            public_key: public_key.to_string(),
+            address: format!("{:?}", addr),
+        }
+    }
+
+    pub fn save_to_file(&self, file_path: &str) -> Result<()> {
+        let file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(file_path)?;
+        let buf_writer = BufWriter::new(file);
+        serde_json::to_writer_pretty(buf_writer, self)?;
+        Ok(())
+    }
+
+    pub fn from_file(file_path: &str) -> Result<Wallet> {
+        let file = OpenOptions::new().read(true).open(file_path)?;
+        let buf_reader: BufReader<std::fs::File> = BufReader::new(file);
+        let wallet: Wallet = serde_json::from_reader(buf_reader)?;
+        Ok(wallet)
+    }
+
+    pub fn get_secret_key(&self) -> Result<SecretKey> {
+        let secret_key = SecretKey::from_str(&self.secret_key)?;
+        Ok(secret_key)
+    }
+    pub fn get_public_key(&self) -> Result<PublicKey> {
+        let pub_key = PublicKey::from_str(&self.public_key)?;
+        Ok(pub_key)
+    }
+
+    pub async fn get_balance(&self, web3_connection: &Web3<WebSocket>) -> Result<U256> {
+        let wallet_address = Address::from_str(&self.address)?;
+        let balance = web3_connection.eth().balance(wallet_address, None).await?;
+        Ok(balance)
+    }
 }
 
 pub fn generate_keypair() -> (SecretKey, PublicKey) {
@@ -39,13 +86,23 @@ pub fn address_from_pubkey(pub_key: &PublicKey) -> Address {
     Address::from_slice(&hash[12..])
 }
 
-impl Wallet {
-    pub fn new(secret_key: &SecretKey, public_key: &PublicKey) -> Self {
-        let addr: Address = address_from_pubkey(&public_key);
-        Wallet {
-            secret_key: secret_key.to_string(),
-            public_key: public_key.to_string(),
-            address: format!("{:?}", addr),
-        }
-    }
+pub async fn establish_web3_connection(url: &str) -> Result<Web3<WebSocket>> {
+    let transport = web3::transports::WebSocket::new(url).await?;
+    Ok(web3::Web3::new(transport))
+}
+
+pub async fn sign_and_send(
+    web3: &Web3<transports::WebSocket>,
+    transaction: TransactionParameters,
+    secret_key: &SecretKey,
+) -> Result<H256> {
+    let signed = web3
+        .accounts()
+        .sign_transaction(transaction, secret_key)
+        .await?;
+    let transaction_result = web3
+        .eth()
+        .send_raw_transaction(signed.raw_transaction)
+        .await?;
+    Ok(transaction_result)
 }
